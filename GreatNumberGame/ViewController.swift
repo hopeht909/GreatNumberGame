8ca02b317c920cd7adf74bdcea9c4ad89cbc07ef
hopeht909 <hopeht909@gmail.com>
2021-12-01 12:05:59 +0000

    Great Number Game

--- GreatNumberGame/ViewController.swift
+++ GreatNumberGame/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    let randomNumber = Int.random(in: 1...100)
+  
+    @IBAction func textFieldContent(_ sender: UITextField) {
+    }
+    @IBAction func submitButtonPressed(_ sender: UIButton) {
+        
+        guessNumber()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        guessNumber()
     }
-
-
+    func showAlertIncorrect(message : String){
+        let alert = UIAlertController(title: "Incorrect", message: message, preferredStyle: .alert)
+        alert.view.tintColor = UIColor.black
+        alert.view.backgroundColor = UIColor.red
+        
+        alert.addAction(UIAlertAction(title: "Guess again", style: .cancel, handler: { action in
+            print("Incorrect")
+        }))
+        present(alert, animated: true)
+    }
+    func showAlertCorrect(){
+        let alert = UIAlertController(title: "Correct", message: "\(numberTextField.text!) was correct!!!", preferredStyle: .alert)
+        alert.view.tintColor = UIColor.black
+        alert.view.backgroundColor = UIColor.green
+        alert.addAction(UIAlertAction(title: "Play again", style: .cancel, handler: { action in
+            print("Correct")
+        }))
+        present(alert, animated: true)
+    }
+    func guessNumber(){
+        if let text = numberTextField.text {
+            if let textInt = Int(text) {
+                
+                if(textInt == randomNumber){
+                    showAlertCorrect()
+                    numberTextField.text = ""
+                }
+                else{
+                    var answer = ""
+                    
+                    if(textInt > randomNumber){
+                        
+                        answer = "\(textInt) Is Too High"
+                    }
+                    else {
+                        answer = "\(textInt) Is Too Low"
+                    }
+                    numberTextField.text = ""
+                    showAlertIncorrect(message: answer)
+                    
+                }
+                
+            }}}
+    
 }
 
